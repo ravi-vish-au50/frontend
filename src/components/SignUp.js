b6ae2486b5ef1c204dadebcf424ab47a812ccf3b
ravi-vish-au50 <110217289+ravi-vish-au50@users.noreply.github.com>
2023-04-27 12:48:15 +0530

    SignUp.js
    Update

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -31,7 +31,7 @@ export default function SignUp() {
     }
 
     // Sending data to server
-    fetch("https://instagram1-clone.onrender.com/signup", {
+    fetch("http://localhost:5000/signup", {
       method: "post",
       headers: {
         "Content-Type": "application/json"
