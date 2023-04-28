aa341735511e3b6b92b813e459d625d9bf84fbd0
ravi-vish-au50 <110217289+ravi-vish-au50@users.noreply.github.com>
2023-04-28 06:07:01 +0530

    MyFollowingPost.js
    Update

--- src/components/MyFollowingPost.js
+++ src/components/MyFollowingPost.js
@@ -22,7 +22,7 @@ export default function MyFolliwngPost() {
     }
 
     // Fetching all posts
-    fetch("http://localhost:5000/myfollwingpost", {
+    fetch("https://instagram1-clone.onrender.com/myfollwingpost", {
       headers: {
         Authorization: "Bearer " + localStorage.getItem("jwt"),
       },
